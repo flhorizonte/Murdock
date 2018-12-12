7fa44dadf5166f7a469f3cc46a6460be4a19d00a
Felipe Horizonte <felipe.hrtz2@gmail.com>
2018-12-12 19:28:44 -0200

    Arquivo de request do cadastro de áreas cienticias

---
+++ app/request/requestScientificAreaRegister.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Request;
+
+require_once("../../config.php");
+
+try {
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		$title = $_POST["title"];
+
+		$model = new ScientificAreaModel();
+		$model->setTitle($title);
+
+		$register = new Register(new ScientificArea)
+
+	} else {
+
+		throw new Exception("Error Processing Request");
+		
+	}
+
+} catch (Exception $e) {
+
+	echo json_encode([
+		"Message" => $e->getMessage()
+	]);
+}
