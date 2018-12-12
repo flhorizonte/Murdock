f13a0756a0575929ba0cf44816d5a628529b624a
Felipe Horizonte <felipe.hrtz2@gmail.com>
2018-12-12 19:33:42 -0200

    Parâmetros da query adicionados

--- src/Users/Login.php
+++ src/Users/Login.php
@@ -2,13 +2,15 @@
 
 namespace Source\Users; 
 
-class Login extends \Model {
+class Login extends \UserModel {
 
 	public function login() {
 
 		$sql = new App\driver\Driver();
 		//replace for join as soon as possible
-		$stmt = $sql->query("SELECT * FROM user WHERE email = :email AND senha = :senha",[$this->getEmail(),$this->getSenha()]); 
+		$stmt = $sql->query("SELECT * FROM user WHERE email = :email AND senha = :senha",[
+			":email" => $this->getEmail(), ":senha" => $this->getSenha()
+		]); 
 		$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 		if(count($data[0]) > 0) {
@@ -16,6 +18,5 @@ class Login extends \Model {
 		} else {
 			throw new Exception("Email e/ou senha incorretos.");
 		}
-
 	}
 }
