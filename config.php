470f3e0353fb8c89fb71a91020c7a9a387c9857c
Felipe Horizonte <felipe.hrtz2@gmail.com>
2018-12-12 19:32:55 -0200

    namespaces adicionados

--- config.php
+++ config.php
@@ -3,3 +3,24 @@
 session_start();
 
 require_once("vendor/autoload.php");
+
+//user model
+use Source\Users\Model as UserModel;
+
+//User
+use Source\Users\Login as Login;
+use Source\Users\Register as UserRegister;
+
+//Article
+use Source\Articles\Article\Article as Article;
+use Source\Articles\Category\Category as Category;
+use Source\Articles\ScientificArea\ScientificArea as ScientificArea;
+use Source\Articles\SubCategory\SubCategory as SubCategory;
+
+use Source\Articles\Register as Register;
+
+//Article models
+use Source\Articles\Article\Model as ArticleModel;
+use Source\Articles\Category\Model as CategoryModel;
+use Source\Articles\ScientificArea\Model as ScientificAreaModel;
+use Source\Articles\SubCategory\Model as SubCategoryModel;
