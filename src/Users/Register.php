b8178f54c86b302023d38dd46f5b4285f92d698f
Felipe Horizonte <felipe.hrtz2@gmail.com>
2018-12-12 19:34:08 -0200

    Parâmetros da query adicionados

--- src/Users/Register.php
+++ src/Users/Register.php
@@ -2,14 +2,14 @@
 
 namespace Source\Users;
 
-class Register extends \Model {
+class Register extends \UserModel {
 
 	public function register() {
 
 		$sql = new App\driver\Driver();
 
-		$stmt = $sql->query("INSERT INTO user (name,email,pass,permission_id,city_id,city_state_id,city_state_country_id) VALUES ({$this->getName()},{$this->getEmail()},{$this->getPass()},{$this->getIdpermission()},{$this->getCity()},{$this->getState()},{$this->getCountry()}	
-		)");
+		$stmt = $sql->query("INSERT INTO user (name,email,pass,permission_id,city_id,city_state_id,city_state_country_id) VALUES (:name,:email,:pass,:idperm,:city,:state,:country)",
+			[":name" => $this->getName(),":email" => $this->getEmail(),":pass" => $this->getPass(),":idperm" => $this->getIdpermission(),":city" => $this->getCity(),":state" => $this->getState(),":country" => $this->getCountry()]);
 
 		if(count($stmt) > 0) {
 
