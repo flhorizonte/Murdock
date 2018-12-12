acba9a3fbeba9e94d9082af397755755bff44235
Felipe Horizonte <felipe.hrtz2@gmail.com>
2018-12-12 13:36:26 -0200

    Classe onde contem os dados dos Usuarios

---
+++ src/Users/Model.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Source\Users;
+
+class Model {
+
+	public $id;
+	public $idpermission;
+	public $city;
+	public $state;
+	public $country;
+	public $name;
+	public $email;
+	public $pass;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     *
+     * @return self
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $state
+     *
+     * @return self
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     *
+     * @return self
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     *
+     * @return self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPass()
+    {
+        //implement pass encryption
+        return $this->pass;
+    }
+
+    /**
+     * @param mixed $pass
+     *
+     * @return self
+     */
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdpermission()
+    {
+        return $this->idpermission;
+    }
+
+    /**
+     * @param mixed $idpermission
+     *
+     * @return self
+     */
+    public function setIdpermission($idpermission)
+    {
+        $this->idpermission = $idpermission;
+    }
+}
